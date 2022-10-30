272d755c80655476d65b282e436fae2ed4f06ce4
Nebula <github@itsnebula.net>
2022-10-30 13:18:01 -0400

    for real

---
+++ main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("java moment");
+        for(String s : args){
+            System.out.println(s);
+        }
+    }
+}
